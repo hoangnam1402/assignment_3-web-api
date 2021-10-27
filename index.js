3c1a819fd8e5d991b548f691d5a1cf1e48b6aac6
hoangnam1402 <18521130@gm.uit.edu.vn>
2021-10-27 19:09:09 +0700

    ...

--- index.js
+++ index.js
@@ -9,8 +9,12 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use(bodyParser.urlencoded({ extended: false}));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+
+app.get('/',(req, res) =>{
+    res.send('Welcome');
+})
 
 require('./app/products')(app);
 
