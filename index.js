47de84f8c9b674d4999854e1cb9013e05da72420
hoangnam1402 <18521130@gm.uit.edu.vn>
2021-10-27 14:05:44 +0700

    hello 2

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const app = express();
+
 const port = process.env.PORT || 3000;
 
 app.get('/',(req,res)=>{
