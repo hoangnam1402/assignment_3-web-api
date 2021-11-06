065c8fb3ae63db2452fdffd3995b8c1f7def4bb2
hoangnam1402 <18521130@gm.uit.edu.vn>
2021-11-06 22:48:34 +0700

    testing

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
 app.get('/',(req, res) =>{
